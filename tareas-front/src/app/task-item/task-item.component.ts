2b714c514494f24ce6e6b6d410168385cba8c601
Eloy Ramirez <clustersnake45@gmail.com>
2025-03-14 00:12:28 -0400

    feat: Crear componentes para manejar las tareas

--- tareas-front/src/app/task-item/task-item.component.ts
+++ tareas-front/src/app/task-item/task-item.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TaskService } from '../task.service';
+import { CommonModule, JsonPipe } from '@angular/common';
+import TaskType from '../types/TaskType';
+
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [JsonPipe, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
+  task = input.required<TaskType>();
+
+  constructor(private taskService:TaskService){
+  }
+
+  checkDone(id:number){
+    //this.taskService.checkDone(id).subscribe()
+    alert('done: ' + id)
+  }
+
+  delete(id:number){
+    alert('delete: ' + id)
+
+  }
+
 
 }
--- tareas-front/src/app/task-list/task-list.component.ts
+++ tareas-front/src/app/task-list/task-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../task.service';
+import { JsonPipe } from '@angular/common';
+import TaskType from '../types/TaskType';
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskItemComponent],
+  imports: [TaskItemComponent, JsonPipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  tasks: TaskType[] = [];
+  constructor(private taskService:TaskService){}
 
+  ngOnInit(){
+    this.taskService.getTasks().subscribe((data:any)=>{
+      this.tasks = Object.keys(data).includes('resultss') ? data.results : [
+        {id: 1, titulo: 'uno', descripcion: 'primero', estado: 'pendiente'},
+        {id: 2, titulo: 'dos', descripcion: 'segundo', estado: 'pendiente'},
+        {id: 3, titulo: 'tres', descripcion: 'tercero', estado: 'completado'}]
+    })
+  }
 }
