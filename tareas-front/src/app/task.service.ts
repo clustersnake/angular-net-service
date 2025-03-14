b47ba06c7bb14971cf99c8eb217eaf86973de48c
Eloy Ramirez <clustersnake45@gmail.com>
2025-03-14 00:06:19 -0400

    feat: Crear servicio para consumir los recursos web

---
+++ tareas-front/src/app/task.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import TaskType from './types/TaskType';
+import TaskResponseType from './types/TaskResponseType';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  apiurl = 'https://pokeapi.co/api/v2/pokemon'
+
+  constructor(private http: HttpClient) {}
+
+  createTask(task: TaskType):Observable<TaskResponseType> {
+    return this.http.post<TaskResponseType>(this.apiurl, task)
+  }
+  getTasks():Observable<TaskResponseType> {
+    return this.http.get<TaskResponseType>(this.apiurl)
+  }
+  checkTask(id:number):Observable<boolean> {
+    return this.http.get<boolean>(this.apiurl)
+  }
+  deleteTask(id: number):Observable<boolean> {
+    return this.http.get<boolean>(this.apiurl)
+  }
+}
