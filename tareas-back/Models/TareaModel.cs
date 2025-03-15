72e0834a59f59bf382e0e34a52b39ce6d62ec485
Eloy Ramirez <clustersnake45@gmail.com>
2025-03-15 10:42:53 -0400

    fix: Ajustar campos del modelo

--- tareas-back/Models/TareaModel.cs
+++ tareas-back/Models/TareaModel.cs
@@ -3,6 +3,7 @@
 public class TareaModel
 {
   public int Id { get; set; }
+  public required string Titulo { get; set; }
   public string? Descripcion { get; set; }
-  public bool Estado { get; set; }
+  public Boolean Completada { get; set; }
 }
